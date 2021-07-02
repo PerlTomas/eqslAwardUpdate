94a34688e3bcb3dd697159fccade86b1b9ecd245
PerlTomas <45515200+PerlTomas@users.noreply.github.com>
2021-07-02 08:03:40 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,14 @@
+This python script will help you update all of your awards in eqsl. You do not have to click on more than 100 hyperlinks anymore :-)
+The script is made for Firefox but should easily be modified for other browsers following the instructions on selenium webpage referenced below.
+
+Prerequisites:
+
+1. Install Selenium. See https://selenium-python.readthedocs.io/installation.html
+2. Install webdriver for your browser described in the same page
+3. Find your HamID number used by eqsl. If you go to the award page and copy the hyperlink to 'Standings' on any award, the number is embedded there after the HamIDTo= tag.
+Like this: https://www.eqsl.cc/QSLCard/eAwardStandings.cfm?CallsignTo=yourcallsign*&HamIDTo=yournumber&Endorsement=eAsia
+
+Call to function:
+python bot("yourusername","yourpassword","yourcallsign", "yourhamid")
+
+It will take a couple of minutes to run the script since there is some delay between calls to each award. This is done to reduce the server load.
---
+++ eqsl.py
@@ -0,0 +1,51 @@
+# https://selenium-python.readthedocs.io/index.html
+
+from selenium import webdriver # Used to import the driver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.firefox.options import Options
+options = Options()
+options.add_argument('--headless') # Hides the browser window
+
+import time
+import random
+
+def bot(usr,pas,callsign, hamid):
+    
+    br=webdriver.Firefox(options=options) # Modify this line if you are using other browser than Firefox
+    br.get("https://www.eqsl.cc/QSLCard/Home4G.cfm")
+    user=br.find_element_by_name("Callsign") 
+    user.clear()
+    user.send_keys(usr) # Fill the user box by given username
+    pasd=br.find_element_by_name("EnteredPassword")
+    pasd.clear()
+    pasd.send_keys(pas) # Fill the password box with password 
+    btn=br.find_element_by_name("Login")
+    btn.click() # Click the button 
+
+    time.sleep(10) # Not sure if this is necessary but we are not in a hurry are we?
+
+    baseurl="https://www.eqsl.cc/QSLCard/eAwardStandings.cfm?CallsignTo="+callsign+"&HamIDTo="+hamid+"&Endorsement="
+    # Read file with award names
+    AwardFile=open("AwardNames.txt",'r')
+    Awards=AwardFile.readlines()
+    AwardFile.close()
+    AwardList=[]
+    
+    for Award in Awards:
+        AwardList.append(Award.strip()) # Remove \n
+    
+    for Award in AwardList: 
+        jitter=random.randint(5,10) # Jitter the time between calls
+        print(Award)
+        br.get(baseurl+Award)
+        try: # Handle 'exceptions' https://stackoverflow.com/questions/19200497/python-selenium-webscraping-nosuchelementexception-not-recognized
+            updatebtn=br.find_element_by_css_selector("input[value='Update MyAwards Page']")
+            updatebtn.click()
+        except NoSuchElementException:
+            print(Award+' not updated')
+        time.sleep(jitter) # Wait between calls to not overload server
+    br.quit()
+#main driver code
+
+bot("yourusername","yourpassword","yourcallsign", "yourhamid")
+
